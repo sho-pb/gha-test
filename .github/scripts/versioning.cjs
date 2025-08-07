a2616106486726102e8daf4e42a84beea4046dbc
sho-pb <shota.tagawa@plan-b.co.jp>
2025-08-07 11:05:19 +0900

    fix

--- .github/scripts/versioning.cjs
+++ .github/scripts/versioning.cjs
@@ -9,7 +9,7 @@
   const payload = {
     tag_name: TRUNKVER,
     name: TRUNKVER,
-    target_commitish: 'prod',
+    target_commitish: 'main',
     generate_release_notes: true,
     draft: false,
     prerelease: false,
